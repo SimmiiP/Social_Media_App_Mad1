507294860a86093716ce47d5cce028d3b82c01b6
Simisola Popoola <20088705@mail.wit.ie>
2023-02-08 16:44:44 +0000

    Initial Comit
    Initial commit

---
+++ app/src/main/java/ie/setu/social_media_app_mad1/SocialMediaActivity.kt
@@ -0,0 +1,11 @@
+package ie.setu.social_media_app_mad1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SocialMediaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_socialmedia) //hrrgubo
+    }
+}
