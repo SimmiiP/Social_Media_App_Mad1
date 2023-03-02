da1b55c359de0fcb2834b6430f76e5e68b37b5c3
Simisola Popoola <20088705@mail.wit.ie>
2023-03-02 03:46:18 +0000

    Image added and added to cards

--- app/src/main/java/ie/setu/social_media_app_mad1/helpers/ImageHelpers.kt
+++ app/src/main/java/ie/setu/social_media_app_mad1/helpers/ImageHelpers.kt
@@ -1,2 +1,13 @@
 package ie.setu.social_media_app_mad1.helpers
 
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import ie.setu.social_media_app_mad1.R
+
+fun showImagePicker(intentLauncher : ActivityResultLauncher<Intent>) {
+    var chooseFile = Intent(Intent.ACTION_OPEN_DOCUMENT)
+    chooseFile.type = "image/*"
+    chooseFile = Intent.createChooser(chooseFile, R.string.select_profile_image.toString())
+    intentLauncher.launch(chooseFile)
+}
+
