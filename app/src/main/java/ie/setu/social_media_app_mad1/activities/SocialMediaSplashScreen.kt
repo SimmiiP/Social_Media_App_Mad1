0ebea22e6dd63f42cc941815e8503456729014e0
Simisola Popoola <20088705@mail.wit.ie>
2023-05-04 01:51:33 +0100

    Night Mode Switch Added

--- app/src/main/java/ie/setu/social_media_app_mad1/activities/SocialMediaSplashScreen.kt
+++ app/src/main/java/ie/setu/social_media_app_mad1/activities/SocialMediaSplashScreen.kt
@@ -3,7 +3,9 @@ package ie.setu.social_media_app_mad1.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import ie.setu.social_media_app_mad1.R
 import ie.setu.social_media_app_mad1.views.socialMediaList.SocialMediaListView
 
@@ -14,11 +16,28 @@ class SocialMediaSplashScreen : AppCompatActivity(){
         setContentView(R.layout.splash_screen)
 
         val startButton = findViewById<Button>(R.id.button)
+        val switchDarkMode = findViewById<Switch>(R.id.switchDarkMode)
         startButton.setOnClickListener{
             val intent = Intent(this, SocialMediaListView::class.java)
             startActivity(intent)
             finish()
         }
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.Theme_Social_Media_App_Mad1)
+        } else{
+            setTheme(R.style.Theme_Social_Media_App_Mad1)
+        }
+
+        switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+        }
+
     }
 
 }
